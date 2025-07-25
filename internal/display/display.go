56af443cbb2df9c5a4675cfdbde275427e264a99
abdorrahmani <mohammad02abdorrahmani@gmail.com>
2025-07-25 23:25:37 +0330

    ✨ feat(display): enhance welcome message with ASCII art and additional branding

--- internal/display/display.go
+++ internal/display/display.go
@@ -5,6 +5,15 @@ import (
 )
 
 func WelcomeMessage() {
-	color.Cyan("DevShare - Share your dev environment over LAN")
-	color.Blue("A tool by Anophel (https://anophel.com)")
+	cliName := `
+ ____              _____ _                    
+|  _ \  _____   __/ ___|| |__   __ _ _ __ ___ 
+| | | |/ _ \ \ / /\___ \| '_ \ / _ | '__/ _ \
+| |_| |  __/\ V /  ___) | | | | (_| | | |  __/
+|____/ \___| \_/  |____/|_| |_|\__,_|_|  \___|
+`
+
+	color.Blue(cliName)
+	color.Green("DevShare - Share your dev environment over LAN")
+	color.Blue("🔗 Served by DevShare - Built with 💙 by Anophel")
 }
